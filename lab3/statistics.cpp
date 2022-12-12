b525d3e78e05fff31aa4c513a64f29872d44b6fd
Francesco Cerroni <francescocerroni03@gmail.com>
2022-12-12 14:38:09 +0100

    sigma migliorata

--- lab3/statistics.cpp
+++ lab3/statistics.cpp
@@ -23,7 +23,9 @@ class Sample {
 
   Statistics statistics() const {
     double mean = sum_x_ / N_;
-    double sigma = std::sqrt(sum_x2_ / N_ - mean * mean);
+    // double sigma = std::sqrt(sum_x2_ / N_ - mean * mean); vecchia funzione
+    // sigma
+    double sigma = std::sqrt(((sum_x2_ / N_ - mean * mean) * N_) / (N_ - 1));
     double mean_err = sigma / std::sqrt(N_);
 
     return {mean, sigma, mean_err};
@@ -38,6 +40,9 @@ TEST_CASE("Testing the class handling a floating point data sample") {
 
   auto result = sample.statistics();
   CHECK(result.mean == doctest::Approx(1.5));
-  CHECK(result.sigma == doctest::Approx(0.5));
-  CHECK(result.mean_err == doctest::Approx(0.35355339));
+  // CHECK(result.sigma == doctest::Approx(0.5)); vecchia funzione sigma
+  CHECK(result.sigma == doctest::Approx(0.70710678));
+  // CHECK(result.mean_err == doctest::Approx(0.35355339)); vecchia funzione
+  // sigma
+  CHECK(result.mean_err == doctest::Approx(0.5));
 }
